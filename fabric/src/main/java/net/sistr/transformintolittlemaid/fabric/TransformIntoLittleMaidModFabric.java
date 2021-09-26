9d9577ecb5e93b030097118e4ddfdd812337954f
sistr <game.sistr@gmail.com>
2021-09-26 13:56:39 +0900

    不要ファイル削除、Mod情報追加

---
+++ fabric/src/main/java/net/sistr/transformintolittlemaid/fabric/TransformIntoLittleMaidModFabric.java
@@ -0,0 +1,10 @@
+package net.sistr.transformintolittlemaid.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TransformIntoLittleMaidModFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ forge/src/main/java/net/sistr/transformintolittlemaid/forge/TransformIntoLittleMaidModForge.java
@@ -0,0 +1,7 @@
+package net.sistr.transformintolittlemaid.forge;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("transformintolittlemaid")
+public class TransformIntoLittleMaidModForge {
+}
