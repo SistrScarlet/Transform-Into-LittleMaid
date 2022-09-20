c4c71e3cc999f6359fde8d7812668bb9d5dd5d4a
sistr <game.sistr@gmail.com>
2022-09-20 12:40:21 +0900

    同期されないのを修正

--- common/src/main/java/net/sistr/transformintolittlemaid/mixin/MixinPlayerEntity.java
+++ common/src/main/java/net/sistr/transformintolittlemaid/mixin/MixinPlayerEntity.java
@@ -109,7 +109,8 @@ public abstract class MixinPlayerEntity extends LivingEntity implements IHasMult
             prevTransformedLittleMaid = bool;
             calculateDimensions();
         }
-        if (world.isClient && syncMultiModel) {
+        if (world.isClient && !syncMultiModel) {
+            syncMultiModel = true;
             RequestSyncMultiModelPacket.sendC2SPacket((PlayerEntity) (Object) this);
         }
     }
