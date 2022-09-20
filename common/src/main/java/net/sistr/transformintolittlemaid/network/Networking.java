5439f9fa94fccc8103105d7bdba3f6ea01bf09f9
sistr <game.sistr@gmail.com>
2022-09-20 13:11:34 +0900

    RequestSyncMultiModelPacketをS2Cにしてたの修正

--- common/src/main/java/net/sistr/transformintolittlemaid/network/Networking.java
+++ common/src/main/java/net/sistr/transformintolittlemaid/network/Networking.java
@@ -1,16 +1,12 @@
 package net.sistr.transformintolittlemaid.network;
 
 import dev.architectury.networking.NetworkManager;
-import dev.architectury.platform.Platform;
-import net.fabricmc.api.EnvType;
 
 public class Networking {
 
     public static void init() {
         NetworkManager.registerReceiver(NetworkManager.Side.C2S, TransformLittleMaidPacket.ID, TransformLittleMaidPacket::receiveC2SPacket);
-        if (Platform.getEnv() == EnvType.CLIENT) {
-            NetworkManager.registerReceiver(NetworkManager.Side.S2C, RequestSyncMultiModelPacket.ID, RequestSyncMultiModelPacket::receiveC2SPacket);
-        }
+        NetworkManager.registerReceiver(NetworkManager.Side.C2S, RequestSyncMultiModelPacket.ID, RequestSyncMultiModelPacket::receiveC2SPacket);
     }
 
 }
